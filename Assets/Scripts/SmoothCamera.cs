2adb083dea01be61ba01d7f46c881544ba922455
Gdziok <Gdziok@uw.edu>
2016-04-30 12:12:29 -0700

    working on a better camera

--- Assets/Scripts/SmoothCamera.cs
+++ Assets/Scripts/SmoothCamera.cs
@@ -8,16 +8,34 @@ public class SmoothCamera : MonoBehaviour
     public float smoothTimeX;
 
     public GameObject player;
-
+    private Vector3 facing;
+    private float headingAngle;
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        facing = player.transform.forward;
+        facing.z = 0;
+        headingAngle = Quaternion.LookRotation(facing).eulerAngles.z;
+        if (headingAngle > 180f) headingAngle -= 360f;
+
     }
 
-    void Update() {
-        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+    void FixedUpdate() {
+        /*  float posX;
+          float posY;
+          if (headingAngle > 0)
+          {
+              posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + 10, ref velocity.x, smoothTimeX);
+              posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+          }
+          else
+          {
+              posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x - 10, ref velocity.x, smoothTimeX);
+              posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+          }*/
 
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        flaot posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
         transform.position = new Vector3(posX, posY, transform.position.z);
     }
 }
