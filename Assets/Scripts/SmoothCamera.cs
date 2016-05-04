62bf4c4a6c17fa26436c615db8a77b4bb40ab5e8
Gdziok <Gdziok@uw.edu>
2016-05-03 21:52:07 -0700

    Better Camera
    Camera will now update to between the mouse cursor and the player

--- Assets/Scripts/SmoothCamera.cs
+++ Assets/Scripts/SmoothCamera.cs
@@ -9,9 +9,6 @@ public class SmoothCamera : MonoBehaviour
 
     public GameObject player;
 
-    private int headingAngle;
-    private int prevHeadingAngle;
-
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -19,59 +16,22 @@ public class SmoothCamera : MonoBehaviour
   
     void FixedUpdate()
     {
-        MouseCamera();
+        LerpCamera();
     }
-    public Vector3 LerpByDistance(Vector3 A, Vector3 B, float x)
-    {
-        Vector3 P = x * Vector3.Normalize(B - A) + A;
-        return P;
-    }
-    void MouseCamera()
+    void LerpCamera()
     {
         float posX;
         float posY;
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,0);
-        posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + (Input.mousePosition.x / Screen.width), ref velocity.x, smoothTimeX);
-        posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + (Input.mousePosition.y / Screen.height), ref velocity.y, smoothTimeY);
 
+      
 
+        Vector3 mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseVector.z = 0;
+        Vector3 newCameraLocation = Vector3.Lerp(player.transform.position, mouseVector, .5f);
+        
+        posX = Mathf.SmoothDamp(transform.position.x, newCameraLocation.x, ref velocity.x, smoothTimeX);
+        posY = Mathf.SmoothDamp(transform.position.y, newCameraLocation.y, ref velocity.y, smoothTimeY);
         transform.position = new Vector3(posX, posY, transform.position.z);
     }
-    void TiedCamera()
-    {
-        float posX;
-        float posY;
-        prevHeadingAngle = headingAngle;
-        headingAngle = player.GetComponent<PlayerForces>().absHor;
-        if (headingAngle > 0)
-        {
-            if (prevHeadingAngle == headingAngle)
-            {
-                posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + 10, ref velocity.x, smoothTimeX);
-                posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
-            }
-            else
-            {
-                posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + 10, ref velocity.x, .6f);
-                posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, .6f);
-            }
-        }
-        else
-        {
-            if (headingAngle == prevHeadingAngle)
-            {
-                posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x - 10, ref velocity.x, smoothTimeX);
-                posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
-            }
-            else
-            {
-                posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x - 10, ref velocity.x, .6f);
-                posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, .6f);
-            }
-        }
-
-        // float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-        //flaot posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
-        transform.position = new Vector3(posX, posY, transform.position.z);
-    }
+    
 }
