1fbd1239c26f3c355722cfad1443e46ea1ebb86b
Eric Soler <e.j.soler1@gmail.com>
2016-05-13 19:04:56 -0700

    changed-prefab-alittle

--- Assets/Scripts/Enviroment/Field.cs
+++ Assets/Scripts/Enviroment/Field.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Field : MonoBehaviour
 {
@@ -8,12 +9,22 @@ public class Field : MonoBehaviour
     //how hard player is pushed or pulled
     public float m_fieldStrength;
 
+    struct GODeltaTuple
+    {
+        public GameObject obj;
+        public Vector2 lastDelta;
+    }
+
+    List<GODeltaTuple> aplyingTo = new List<GODeltaTuple>();
+
+
     void Start() {
     }
 
     void Update() {
 
     }
+
 
     public void setPolarity(float _polarity) {
         m_fieldStrength = _polarity;
@@ -24,11 +35,19 @@ public class Field : MonoBehaviour
             Vector2 addVel = this.transform.up * m_fieldStrength
                 * other.GetComponent<PlayerBase>().m_PlayerPolarity;
             other.GetComponent<PlayerBase>().playerForces.addToDelta(addVel);
+            GODeltaTuple tup = new GODeltaTuple();
+            tup.obj = other.gameObject;
+            tup.lastDelta = addVel;
+            aplyingTo.Add(tup);
         }
         if (other.gameObject.tag == "Enemy") {
             Vector2 addVel = this.transform.up * m_fieldStrength
                 * other.GetComponent<EnemyBase>().m_PlayerPolarity;
             other.GetComponent<EnemyBase>().playerForces.addToDelta(addVel);
+            GODeltaTuple tup = new GODeltaTuple();
+            tup.obj = other.gameObject;
+            tup.lastDelta = addVel;
+            aplyingTo.Add(tup);
         }
     }
 
@@ -36,40 +55,47 @@ public class Field : MonoBehaviour
         if (other.gameObject.tag == "Player") {
             Vector2 addVel = this.transform.up * m_fieldStrength
     * other.GetComponent<PlayerBase>().m_PlayerPolarity;
-            other.GetComponent<PlayerBase>().playerForces.addToDelta(addVel *-1);            
+            other.GetComponent<PlayerBase>().playerForces.addToDelta(addVel *-1);
+            int remove = -1;
+            for(int i = 0; i < aplyingTo.Count; i++) {
+                if(aplyingTo[i].obj.GetInstanceID() == other.gameObject.GetInstanceID()) {
+                    remove = i;
+                    break;
+                }
+            }
+            if(remove != -1) {
+                aplyingTo.RemoveAt(remove);
+            }
         }
         else if (other.gameObject.tag == "Enemy") {
             Vector2 addVel = this.transform.up * m_fieldStrength
     * other.GetComponent<EnemyBase>().m_PlayerPolarity;
             other.GetComponent<EnemyBase>().playerForces.addToDelta(addVel * -1);
+            int remove = -1;
+            for (int i = 0; i < aplyingTo.Count; i++) {
+                if (aplyingTo[i].obj.GetInstanceID() == other.gameObject.GetInstanceID()) {
+                    remove = i;
+                    break;
+                }
+            }
+            if (remove != -1) {
+                aplyingTo.RemoveAt(remove);
+            }
+        }
+    }
+
+    void OnDestroy() {
+        foreach(GODeltaTuple ele in aplyingTo) {
+            if (ele.obj.tag == "Player") {
+                ele.obj.GetComponent<PlayerBase>().playerForces.addToDelta(ele.lastDelta * -1);
+            }
+            else if(ele.obj.tag == "Enemy") {
+                ele.obj.GetComponent<EnemyBase>().playerForces.addToDelta(ele.lastDelta * -1);
+            }
         }
     }
 
     void OnTriggerStay2D(Collider2D other) {
-        //if (other.gameObject.tag == "Player") {
-        //    if (other.attachedRigidbody) {
-        //        Vector2 addVel = this.transform.up * m_fieldStrength
-        //            * other.GetComponent<PlayerBase>().m_PlayerPolarity;
-        //        other.attachedRigidbody.velocity = new Vector2(
-        //            other.attachedRigidbody.velocity.x +
-        //            addVel.x, other.attachedRigidbody.velocity.y +
-        //            addVel.y);
-        //        //other.attachedRigidbody.AddForce(
-        //        //    this.transform.up * m_fieldStrength
-        //        //    * other.GetComponent<PlayerBase>().m_PlayerPolarity, 
-        //        //    ForceMode2D.Force);
-        //    }
-        //}
-        //if (other.gameObject.tag == "Enemy") {
-        //    if (other.attachedRigidbody) {
-        //        other.attachedRigidbody.AddForce(
-        //            this.transform.up * m_fieldStrength
-        //            * other.GetComponent<EnemyBase>().m_PlayerPolarity,
-        //            ForceMode2D.Force);
-
-        //        //Debug.Log("here:");
-        //    }
-        //}
     }
 
     public int isPosOrNeg() {
