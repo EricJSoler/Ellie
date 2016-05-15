9442a56c6b67e6d6feefd87ef3a5ca9765fbcab9
hjl0505 <hyunniebucket@gmail.com>
2016-05-14 21:20:25 -0700

    framerate UI

---
+++ Assets/Scripts/UI/UIFrameRate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIFrameRate : MonoBehaviour {
+
+    public Text timer;
+    private double framerate;
+
+    void Start() {
+        framerate = 0f;
+
+        timer = GetComponent<Text>() as Text;
+    }
+
+    void Update() {
+        framerate = 1.0 / (Time.deltaTime);
+        timer.text = "Frame Rate: " + framerate.ToString("00");
+      
+    }
+}
