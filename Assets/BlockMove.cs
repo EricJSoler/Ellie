6a4d30a711e6d36edf42cbd5aa29b58b1d4a3033
Eric Soler <e.j.soler1@gmail.com>
2016-05-25 10:08:13 -0700

    okay?

---
+++ Assets/BlockMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockMove : MonoBehaviour {
+
+    
+    public Vector2[] checkpoints;
+    public int moveSpeed;
+
+    int currentGoal;
+
+    Rigidbody2D m_rBody;
+    float distanceBeforeTurning = .1f;
+    // Use this for initialization
+    void Start() {
+        m_rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Vector3.Distance(transform.position, checkpoints[currentGoal]) <= distanceBeforeTurning) {
+            currentGoal = (currentGoal + 1) % checkpoints.Length;
+        }
+        Vector2 newVel = (checkpoints[currentGoal] - new Vector2(transform.position.x, transform.position.y)).normalized * moveSpeed;
+        m_rBody.velocity = newVel;
+        
+    }
+}
