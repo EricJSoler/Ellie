55b23cc3e4c3945e23f9d73282b6dea5b769c97b
hjl0505 <hyunniebucket@gmail.com>
2016-06-01 12:09:22 -0700

    cleaned up stuff and added GlobalBest, multiplayer UI, and Tutorial(in progress)

---
+++ Assets/Scripts/UI/TutorialNav.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialNav : MonoBehaviour {
+
+    public GameObject[] panels;
+
+	// Use this for initialization
+	void Start () {
+        disablePanels();
+        next(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void next (int index) {
+        disablePanels();
+        panels[index].SetActive(true);
+    }
+
+    public void disablePanels () {
+        for (int i = 0; i < panels.Length; i++) {
+            panels[i].SetActive(false);
+        }
+    }
+}
