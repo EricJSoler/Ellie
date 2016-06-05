c914001d9b6bb876032eda081280c29beb93c1a6
Cyrus Sarkosh <csarkosh@uw.edu>
2016-06-05 12:57:03 -0700

    Fixed highscores format

--- Assets/DB/DisplayHighScores.cs
+++ Assets/DB/DisplayHighScores.cs
@@ -11,12 +11,14 @@ public class DisplayHighScores : MonoBehaviour {
     public Text[] highScoreText;
     private HighScores highScoreManager;
 
+    void Awake()
+    {
+        highScoreManager = GetComponent<HighScores>();
+    }
 
 	// Use this for initialization
 	void Start ()
     {
-        highScoreManager = GetComponent<HighScores>();
-
         for (int i = 0; i < highScoreText.Length; i++)
         {
             highScoreText[i].text = i + 1 + ". Retrieving...";
@@ -27,14 +29,14 @@ public class DisplayHighScores : MonoBehaviour {
 
 
     //--------------------------------- OnHighScoresDownloaded ---------------------------------------
-    // Connects to web URL and downloads highscores for specified level
+    // Displays highscores to screen
     //------------------------------------------------------------------------------------------------
     public void OnHighscoresDownloaded (HighScoresNode[] highScoresList)
     {
         int rank = 0;
         bool lessHighScoreNodes = highScoresList.Length < highScoreText.Length;
 
-        for (int i = 0; i < highScoreText.Length; i++)
+        for (int i = 0; i < highScoreText.Length; i += 2)
         {
             highScoreText[i].text = ++rank + ". ";
 
@@ -44,12 +46,15 @@ public class DisplayHighScores : MonoBehaviour {
                     highScoresList[highScoresList.Length - rank].name 
                         = highScoresList[highScoresList.Length - rank].name.Substring(0, maxNameLength);
 
-                highScoreText[i].text += highScoresList[highScoresList.Length - rank].name
-                    + " - " + highScoresList[highScoresList.Length - rank].time;
+                highScoreText[i].text += highScoresList[highScoresList.Length - rank].name;
+                highScoreText[i + 1].text = formatTime(highScoresList[highScoresList.Length - rank].time);
+                //+ addSpaceBuffer(highScoresList[highScoresList.Length - rank].name.Length)
+                //+ formatTime(highScoresList[highScoresList.Length - rank].time);
             }
             catch (Exception e)
             {
                 highScoreText[i].text += "null";
+                highScoreText[i + 1].text = "";
                 Debug.Log(e);
             }
         }
@@ -57,7 +62,7 @@ public class DisplayHighScores : MonoBehaviour {
 
 
     //--------------------------------- refreshHighscores --------------------------------------------
-    // Connects to web URL and downloads highscores for specified level
+    // Refreshes global highscores
     //------------------------------------------------------------------------------------------------
     private IEnumerator RefreshHighscores()
     {
@@ -66,5 +71,31 @@ public class DisplayHighScores : MonoBehaviour {
             highScoreManager.getHighScores(highScoresLevel);
             yield return new WaitForSeconds(30);
         }
+    }
+
+
+    //--------------------------------- formatTime ---------------------------------------------------
+    // Reformats the float value of time for proper display
+    //------------------------------------------------------------------------------------------------
+    private string formatTime(float time)
+    {
+        string minutes = Mathf.Floor(time / 60) + "";
+        string seconds = time % 60 < 10 ? "0" + time % 60 : (time % 60) + "";
+
+        return minutes + ":" + seconds;
+    }
+
+
+    //--------------------------------- addSpaceBuffer -----------------------------------------------
+    // Adds spaces between name and time
+    //------------------------------------------------------------------------------------------------
+    private string addSpaceBuffer(int nameLength)
+    {
+        string s = "";
+
+        for (int i = 0; i < 10 - nameLength; i++)
+            s += " ";
+
+        return s;
     }
 }
